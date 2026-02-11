25d807989eb2a431e64a4e73a401751f4a17bdeb
Zoë <zkcrisler@gmail.com>
2026-02-10 22:29:28 -0600

    slow gif ^

--- main/programming.js
+++ main/programming.js
@@ -3,7 +3,9 @@ const mainBody = document.getElementById("mainBody")
 function twinkleToggle() {
     if(mainBody.classList.contains("tileStarry")) {
         mainBody.classList.replace("tileStarry","tileStarryAlt")
+    } else if(mainBody.classList.contains("tileStarryAlt")) {
+        mainBody.classList.replace("tileStarryAlt","tileStarryAlt1")
     } else {
-        mainBody.classList.replace("tileStarryAlt","tileStarry")
+        mainBody.classList.replace("tileStarryAlt1","tileStarry")
     }
 }
