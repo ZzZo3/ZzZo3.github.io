aaa4232722474778132d6d6064e1a5fa7d98ba1f
Zoë <zkcrisler@gmail.com>
2026-02-11 17:30:50 +0000

    update photoCount with each element of class dynamicImageData

--- photography/programming.js
+++ photography/programming.js
@@ -1,11 +1,18 @@
 //BASE
 const photoCount = '0'
 const pageTitle = document.getElementsByClassName('pageTitle')
+//DYNAMIC IMAGE DISPLAY
+const dynamicImageDisplay = document.getElementsByClassName('dynamicImageDisplay')
+const dynamicImageRow = document.getElementsByClassName('dynamicImageRow')
+const dynamicImageData = document.getElementsByClassName('dynamicImageData')
 
 //STUFF THAT RUNS ON LOAD
 
 function loadFunc() {
     console.log('\"loadFunc()\" began')
+    dynamicImageData.forEach(img => {
+        photoCount += 1
+    });
     for(let i = 0; i<pageTitle.length; i++) {
         pageTitle[i].textContent = 'Photos! ['+photoCount+']'
     }
