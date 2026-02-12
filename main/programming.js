8a9e16f070d23f9a34ec65bf2664e93f61e48b27
Zoë <zkcrisler@gmail.com>
2026-02-11 18:35:55 -0600

    ^

--- main/programming.js
+++ main/programming.js
@@ -3,26 +3,32 @@ const mainBody = document.getElementById("mainBody")
 
 var CLUBNINE = document.getElementById("CLUBNINE");
 
-var isometricTilesQuery = document.querySelectorAll(".isometricTile");
-isometricTilesQuery.forEach((element) => {
-    console.log(tile.classList)
-    console.log(tile.classList[1])
-    console.log(tile.classList[1].slice(1))
-    console.log(tile.classList[1].slice(1) + "px")
-    console.log('predicted success')
-    let tile = element
-    let x = tile.classList[1].slice(1)
-    let y = tile.classList[2].slice(1)
-    tile.style.left = x + "px";
-    tile.style.top = y + "px";
-    console.log('realized success')
-    return tile
-})
+function positionTiles() {
+    console.log('\"positionTiles()\" began')
+    var isometricTilesQuery = document.querySelectorAll(".isometricTile");
+    isometricTilesQuery.forEach((element) => {
+        console.log(tile.classList)
+        console.log('   '+tile.classList[1])
+        console.log('   '+tile.classList[1].slice(1))
+        console.log('   '+tile.classList[1].slice(1) + "px")
+        console.log('   predicted success')
+        let tile = element
+        let x = tile.classList[1].slice(1)
+        let y = tile.classList[2].slice(1)
+        //translate x's and y's to isometric tiles
+        tile.style.left = x + "px";
+        tile.style.top = y + "px";
+        console.log('   realized success')
+        return tile
+    })
+    console.log('   \"positionTiles()\" finished')
+}
 
 //STUFF THAT RUNS ON LOAD
 
 function loadFunc() {
     console.log('\"loadFunc()\" began')
+    positionTiles()
     console.log('   \"loadFunc()\" finished')
 }
 
