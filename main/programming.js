7883ee664f68a6a02718fd574694da4ed3ccb07b
Zoë <zkcrisler@gmail.com>
2026-02-12 17:31:20 +0000

    ^

--- main/programming.js
+++ main/programming.js
@@ -150,11 +150,11 @@ function renderIsometric() {
 }
 
 function shiftUp() {
-    positionOffsetY -= 11 * tileScale * isoSpread
+    positionOffsetY -= 11 * tileScale * isoSpread + mountainOffset
     renderIsometric()
 }
 function shiftDown() {
-    positionOffsetY += 11 * tileScale * isoSpread
+    positionOffsetY += 11 * tileScale * isoSpread + mountainOffset
     renderIsometric()
 }
 function shiftLeft() {
