f361d8ca6f6cf68e13c54c003cf9f6d5fc402ace
Zoë <zkcrisler@gmail.com>
2026-02-11 22:04:48 -0600

    isometric rendering (broken by something else)

--- main/programming.js
+++ main/programming.js
@@ -1,16 +1,19 @@
 //BASE
 const mainBody = document.getElementById("mainBody")
 
+const isoScale = 2.0
+const tileScale = 2.0
+
 function positionTiles() {
+    console.log('\"positionTiles()\" began')
     console.log('\"positionTiles()\" began')
     var isometricTilesQuery = document.querySelectorAll(".isometricTile");
     isometricTilesQuery.forEach((element) => {
         let tile = element
-        console.log(tile.classList)
-        console.log('   '+tile.classList[1])
-        console.log('   '+tile.classList[1].slice(1))
-        console.log('   '+tile.classList[1].slice(1) + "px")
-        console.log('   predicted success')
+        let off = 0
+        if (tile.classList.length == 4) {
+          off = parseInt(tile.classList[3].slice(3))
+        }
         let xi = parseInt(tile.classList[1].slice(1))
         let yi = parseInt(tile.classList[2].slice(1))
         //translate x's and y's to isometric tiles
@@ -19,15 +22,20 @@ function positionTiles() {
         let xf = 0
         let yf = 0
         for(let step = 1; step <= xi; step++) {
-            xf += 32
-            yf -= 11
+            xf += 32*isoScale
+            yf -= 11*isoScale
         }
         for(let step = 1; step <= yi; step++) {
-            xf += 32
-            yf += 11
+            xf += 32*isoScale
+            yf += 11*isoScale
         }
-        tile.style.left = xf + "px";
-        tile.style.top = yf + "px";
+        //off = yf #mountain idea
+        yf += off * tileScale
+        tile.style.left = xf + "px"
+        tile.style.top = yf + "px"
+        tile.style.zIndex = yi - xi
+        tile.style.width = 64*tileScale + "px"
+        tile.style.height = 256*tileScale + "px"
         console.log('   realized success')
         return tile
     })
