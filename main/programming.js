00a30405afc601b6fc9eae1ef1d13e14312cf835
Zoë <zkcrisler@gmail.com>
2026-02-11 21:19:28 -0600

    ^

--- main/programming.js
+++ main/programming.js
@@ -2,7 +2,7 @@
 const mainBody = document.getElementById("mainBody")
 
 var CLUBNINE = document.getElementById("CLUBNINE");
-/*
+
 function positionTiles() {
     console.log('\"positionTiles()\" began')
     var isometricTilesQuery = document.querySelectorAll(".isometricTile");
@@ -13,17 +13,29 @@ function positionTiles() {
         console.log('   '+tile.classList[1].slice(1))
         console.log('   '+tile.classList[1].slice(1) + "px")
         console.log('   predicted success')
-        let x = tile.classList[1].slice(1)
-        let y = tile.classList[2].slice(1)
+        let xi = parseInt(tile.classList[1].slice(1))
+        let yi = parseInt(tile.classList[2].slice(1))
         //translate x's and y's to isometric tiles
-        tile.style.left = x + "px";
-        tile.style.top = y + "px";
+        //for x (+32,-11) ->^
+        //for y (+32,+11) ->v
+        let xf = 0
+        let yf = 0
+        for(let step = 1; step <= xi; step++) {
+            xf += 32
+            yf -= 11
+        }
+        for(let step = 1; step <= yi; step++) {
+            xf += 32
+            yf += 11
+        }
+        tile.style.left = xf + "px";
+        tile.style.top = yf + "px";
         console.log('   realized success')
         return tile
     })
     console.log('   \"positionTiles()\" finished')
 }
-*/
+
 //STUFF THAT RUNS ON LOAD
 
 function loadFunc() {
