a53baa241a88a54a35e86667441eed8dc6430031
Zoë <zkcrisler@gmail.com>
2026-02-20 01:59:18 +0000

    ^

--- ratex/programming.js
+++ ratex/programming.js
@@ -312,7 +312,7 @@ singleSlash.onclick = ()=>{
     let text = input.value
     text = text.split('\n') //splits text into array of lines
     console.log('   first split worked - text: \n'+text)
-    text = text.map(line=>{line.split('')}) //splits each line into array of characters
+    text = text.map(line=>line.split('')) //splits each line into array of characters
     console.log('   first map worked - text: \n'+text)
     text = text.map((line, num)=>{
         console.log('map started')
