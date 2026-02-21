3a5282df44e495bd4f9dd0c26ba420e84de4d644
Zoë <zkcrisler@gmail.com>
2026-02-21 22:49:27 +0000

    added idle branch

---
+++ idle/programming.js
@@ -0,0 +1,12 @@
+//BASE
+const pageTitle = document.getElementsByClassName('pageTitle')
+
+/* code go here */
+
+//STUFF THAT RUNS ON LOAD
+
+function loadFunc() {
+    console.log('\"loadFunc()\" began')
+    /* code go here */
+    console.log('   \"loadFunc()\" finished')
+}
