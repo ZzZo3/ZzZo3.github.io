4153e175c169eed4dc6d381423fd508b8b4101f7
Zoë <zkcrisler@gmail.com>
2026-02-26 15:23:27 -0600

    added to terminal.js

--- JS/terminal.js
+++ JS/terminal.js
@@ -1,1 +1,19 @@
- 
+//BASE
+const terminalInput = document.getElementById('terminalInput')
+
+//KEY LISTENER
+document.addEventListener('keydown', function (event) {
+    console.log('Key: \"' + event.key + '\"');
+    if (document.activeElement === terminalInput) { // detect Enter / shift+Enter in terminalInput
+        if (event.key === "Enter") {
+            if (!event.shiftKey) {
+                alert('terminal enter')
+            } else {
+                var height = window.getComputedStyle(terminalInput).height
+                var pixels = parseInt(height.replace('px',''))
+                height = (pixels + 18)+'px'
+                terminalInput.style.height = height
+            }
+        }
+    }
+});
