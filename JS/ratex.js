467eebf1be6170d2c45fce3b828652d1748c8918
Zoë <zkcrisler@gmail.com>
2026-03-02 08:50:06 -0600

    RatEx appended to docs

--- JS/ratex.js
+++ JS/ratex.js
@@ -255,7 +255,7 @@ function confirmSaveAsFunc(saveAs) {
             alert('deceitful.')
         }
     } else if(saveAs != '') {
-        localStorage.setItem(saveAs,input.value)
+        localStorage.setItem('RaTeX'+saveAs,input.value)
         displayData()
         console.log('   \"'+saveAs+'\" write success')
         saveAsBox.value = ''
@@ -266,7 +266,7 @@ function confirmSaveAsFunc(saveAs) {
 }
 function confirmPullDataFunc(data) {
     console.log('attempting to read: \"'+data+'\"')
-    if(Object.keys(localStorage).includes(data)) {
+    if(Object.keys(localStorage).includes('RaTeX'+data)) {
         console.log('   \"'+data+'\" found')
         input.value = localStorage[data]
         output.textContent = translate(input.value,false)
