769337d8039a1f83d6eb3eea46750051dcf0668f
Zoë <zkcrisler@gmail.com>
2026-03-03 08:39:33 -0600

    default height for zork terminal

--- JS/notzork.js
+++ JS/notzork.js
@@ -9,5 +9,6 @@ function loadFunc() {
     for(let i = 0; i<pageTitle.length; i++) {
         pageTitle[i].textContent = 'ZzZ0rk! '+vNum
     }
+    document.getElementById('terminal').style.height = (36*18)+'px'
     console.log('   \"loadFunc()\" finished')
 }
