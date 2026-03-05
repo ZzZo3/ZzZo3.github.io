fbaf79dba2f39393677445a5f9bee7bad9a28f2f
Zoë <zkcrisler@gmail.com>
2026-03-05 17:23:25 -0600

    ^

--- JS/terminal.js
+++ JS/terminal.js
@@ -226,16 +226,16 @@ new TerminalCMND(['help'], // HELP
 `
 SYNTAX: \'help [cmnd]\'
 red%cPURPOSES:
-⠀⠀⠀\'help\' blue%cexplains syntax and purpose of a named locally available command.
-⠀⠀⠀\'help\' is globally available.
-⠀⠀⠀ * Some commands are available across all instances of TERMINAL.js while others are only available in specific html files within the
-⠀⠀⠀   n0n-sense.org domain.
+    \'help\' blue%cexplains syntax and purpose of a named locally available command.
+    \'help\' is globally available.
+     * Some commands are available across all instances of TERMINAL.js while others are only available in specific html files within the
+       n0n-sense.org domain.
 ARGUMENTS:
-⠀⠀⠀[cmnd] (optional)* takes name of any command. If ignored, the name(s) of all locally available commands are displayed with an AWAIT statement*.
-⠀⠀⠀ * Optional arguments are automatically ignored if they appear as the last argument in a command and are left blank.
-⠀⠀⠀   They may be manually ignored with \'-\'.
-⠀⠀⠀ * An AWAIT statement is a query, called with acceptable replies for the user to pick from. Any AWAIT statemnet may also be cancelled
-⠀⠀⠀   with \'cancel\'.
+    [cmnd] (optional)* takes name of any command. If ignored, the name(s) of all locally available commands are displayed with an AWAIT statement*.
+     * Optional arguments are automatically ignored if they appear as the last argument in a command and are left blank.
+       They may be manually ignored with \'-\'.
+     * An AWAIT statement is a query, called with acceptable replies for the user to pick from. Any AWAIT statemnet may also be cancelled
+       with \'cancel\'.
 `,
     [new TerminalARG('cmnd',[],true)],
 (argList)=>{
