754312d5ef773a78151bcb6daeccc38d0120cf02
Zoë <zkcrisler@gmail.com>
2026-03-06 07:32:48 -0600

    Update errorAlerts.js

--- JS/errorAlerts.js
+++ JS/errorAlerts.js
@@ -1,9 +1,12 @@
-window.addEventListener("error", handleError, true);
-
-function handleError(evt) {
-    if (evt.message) { // Chrome sometimes provides this
-      alert("error: "+evt.message +" at linenumber: "+evt.lineno+" of file: "+evt.filename);
-    } else {
-      alert("error: "+evt.type+" from element: "+(evt.srcElement || evt.target));
+const errorAlertsActive = false
+if (errorAlertsActive) {
+    window.addEventListener("error", handleError, true);
+    
+    function handleError(evt) {
+        if (evt.message) { // Chrome sometimes provides this
+          alert("error: "+evt.message +" at linenumber: "+evt.lineno+" of file: "+evt.filename);
+        } else {
+          alert("error: "+evt.type+" from element: "+(evt.srcElement || evt.target));
+        }
     }
 }
