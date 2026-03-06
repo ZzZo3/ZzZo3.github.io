19cdc32c63897153eacac1d22f5715fbb8a76b9f
Zoë <zkcrisler@gmail.com>
2026-03-06 10:30:35 -0600

    fixed some color typos

--- JS/terminal.js
+++ JS/terminal.js
@@ -448,7 +448,7 @@ ARGUMENTS:%cred
         'Beige','Bisque','Black','BlanchedAlmond','Blue','BlueViolet','Brown','BurlyWood',
         'CadetBlue','Chartreuse','Chocolate','Coral','CornflowerBlue','Cornsilk','Crimson','Cyan',
         'DarkBlue','DarkCyan','DarkGoldenRod','DarkGray','DarkGreen','DarkKhaki','DarkMagenta',
-        'DarkOliveGreen','DarkOrage','DarkOrchid','DarkRed','DarkSalmon','DarkSeaGreen','DarkSlateBlue',
+        'DarkOliveGreen','DarkOrange','DarkOrchid','DarkRed','DarkSalmon','DarkSeaGreen','DarkSlateBlue',
         'DarkSlateGray','DarkTurquoise','DarkViolet','DeepPink','DeepSkyBlue','DimGray','DodgerBlue','FireBrick',
         'FloralWhite','ForestGreen','Fuchsia','Gainsboro','GhostWhite','Gold','GoldenRod','Gray','Green','GreenYellow',
         'HoneyDew','HotPink','IndianRed','Indigo','Ivory','Khaki','Lavender','LavenderBlush','LawnGreen','LemonChiffon',
@@ -459,7 +459,7 @@ ARGUMENTS:%cred
         'Olive','OliveDrab','Orange','OrangeRed','Orchid','PaleGoldenRod','PaleGreen','PaleTurquoise','PaleVioletRed','PapayaWhip',
         'PeachPuff','Peru','Pink','Plum','Powderblue','Purple','RebeccaPurple','Red','RosyBrown','RoyalBlue','SaddleBrown','Salmon',
         'SandyBrown','SeaGreen','SeaShell','Sienna','Silver','SkyBlue','SlateBlue','SlateGray','Snow','SpringGreen','SteeleBlue',
-        'Tan','Teal','Thisle','Tomato','Turquoise','Violet','Wheat','White','WhiteSmoke','Yellow','YellowGreen']
+        'Tan','Teal','Thistle','Tomato','Turquoise','Violet','Wheat','White','WhiteSmoke','Yellow','YellowGreen']
         colors.forEach((color)=>{
         TERMINAL.write('████████████████████████%c'+color+' '+color)
         })
