32bcb528b05e7678ec0edeea0016b072014ee395
Zoë <zkcrisler@gmail.com>
2026-03-09 16:11:02 -0500

    ^

--- JS/idle.js
+++ JS/idle.js
@@ -4,18 +4,17 @@ const stepButton = document.getElementById('stepButton')
 const nDisplay = document.getElementById('nDisplay')
 // VARIABLES
 var n = 0
-const N = 472
-const tempN = 15
+const N = 15 //472
 
 function cap() {
     alert('no more.')
-    window.open('https://n0n-sense.org/naught')
+    window.open('https://n0n-sense.org/nomore')
 }
 
 stepButton.addEventListener('click', (event)=>{
     n++
-    if (n > tempN) {
-        n = tempN
+    if (n >= N) {
+        n = N
         cap()
     }
     nDisplay.innerText = '['+n+']'
