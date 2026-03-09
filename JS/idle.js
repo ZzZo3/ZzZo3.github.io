c855d10149fc498f3695be8e3292cfce8a1e7bfc
Zoë <zkcrisler@gmail.com>
2026-03-09 16:03:42 -0500

    fomrat

--- JS/idle.js
+++ JS/idle.js
@@ -5,6 +5,7 @@ const nDisplay = document.getElementById('nDisplay')
 // VARIABLES
 var n = 0
 const N = 472
+const tempN = 15
 
 function cap() {
     alert('no more.')
@@ -13,8 +14,8 @@ function cap() {
 
 stepButton.addEventListener('click', (event)=>{
     n++
-    if (n > N) {
-        n = N
+    if (n > tempN) {
+        n = tempN
         cap()
     }
     nDisplay.innerText = '['+n+']'
