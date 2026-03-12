62309929786ac416c7e30e452da399c42ef9f2a1
Zoë <zkcrisler@gmail.com>
2026-03-12 11:01:13 -0500

    ^

--- JS/main.js
+++ JS/main.js
@@ -73,8 +73,9 @@ function setBigBases() {
         let coords = [tile.classList[1],tile.classList[2]]
         BIGBASES.push(new BIGBASE(coords, pts))
     })
+    console.log('BIGBASES:')
+    console.log(BIGBASES)
 }
-function viewBIGS() {console.log(BIGBASES)}
 
 
 //BASE
