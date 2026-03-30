7129f4beedec69f3100e0c725ffccd297ac27116
Zoë <zkcrisler@gmail.com>
2026-03-30 15:33:31 -0500

    zarg

---
+++ JS/zarg.js
@@ -0,0 +1,11 @@
+//BASE
+const vNum = '0.0.0'
+const pageTitle = document.getElementsByClassName('pageTitle')
+
+
+//STUFF THAT RUNS ON LOAD
+
+function loadFunc() {
+    console.log('\"loadFunc()\" began')
+    console.log('   \"loadFunc()\" finished')
+}
