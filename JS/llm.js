598599ac1fbd0ac88e8086ef2d54f8f88750a137
Zoë <zkcrisler@gmail.com>
2026-05-15 18:39:18 -0500

    ^

--- JS/llm.js
+++ JS/llm.js
@@ -14,7 +14,7 @@ const capitalizedWords = ['i','lord','sankara',
   'banquo','hecate','duncan','malcolm','donalbain',
   'macduff','lennox','ross','menteith','agnus',
   'caithness','fleance','siward','seyton','cawdor',
-  'ireland']
+  'ireland','birmingham']
 const punctuation = [",",";",":",".","!","?","[","]","(",")",
   "{","}","<",">","/","\`","~","\'","\"","|","-","—","—","—"]
 const puncTerminating = [".","!","?"]
