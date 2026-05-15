6bbca9f83a54f7613a0913b6bbb78bfd2cfd1805
Zoë <zkcrisler@gmail.com>
2026-05-15 16:55:37 +0000

    ^

--- JS/llm.js
+++ JS/llm.js
@@ -11,18 +11,29 @@ var cNexts = [["a",0.25],["b",0.25],["c",0.25],["\\end",0.25]]
 
 var nextWords = [["a",aNexts],["b",bNexts],["c",cNexts]]
 
-function follow(input) {
-  if (!nextWords.contains(input)) {
-    return "0"
-  }
+function follow(inWord) {
   var index = 0
   for (let i = 0; i<nextWords.length; i++) {
-    if (nextWords[i]==input) {
+    if (nextWords[i][0]==inWord) {
       index = i
     }
   }
   var nexts = nextWords[index][1]
-  var next = nexts[Math.floor(Math.random() * nexts.length)];
-  console.log((input," ",next))
-  return (input," ",next)
+  var outWord = nexts[Math.floor(Math.random() * nexts.length)][0];
+  return (outWord)
 }
+
+function write(inWord) {
+  var text = [inWord]
+  var last = inWord
+  for (let n=0; n<100; n++) {
+    if (last!="\\end") {
+      var next = follow(last)
+      text.push(next)
+      last = next
+    }
+  }
+  return(text.join(" "))
+}
+
+write("a")
